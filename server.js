25f532173c03e39a220fd1390a66d5624d795235
Eric Murphy <ericnmurphy@gmail.com>
2018-10-28 10:55:59 +0700

    Added dotenv

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 //mongoose connect
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 

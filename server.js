cde50ca803445e099282ef5e6f528222cc704063
Eric Murphy <ericnmurphy@gmail.com>
2018-10-28 10:52:44 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+//mongoose connect
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const app = express();
+
+const port = process.env.PORT || 7000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});

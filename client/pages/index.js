903173ca9bc333f1b6109d303e1736b457b47701
Eric Murphy <ericnmurphy@gmail.com>
2018-10-29 09:58:18 +0700

    Added concurrently

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,27 @@
+import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 
 const Index = props => (
   <div>
-    Hello, world.{" "}
+    Hello, world.
     <ul>
       {props.makers.map(maker => (
         <li>
-          <h3>{maker.name}</h3>
+          <h3>
+            {maker.name}{" "}
+            <Link href={{ pathname: `https://twitter.com/${maker.twitter}` }}>
+              <a target="_blank">@{maker.twitter}</a>
+            </Link>
+          </h3>
         </li>
       ))}
     </ul>
   </div>
 );
 
-Index.getInitialProps = async function() {
-  const res = await fetch(`http://localhost:5000/api/makers/all`);
+Index.getInitialProps = async () => {
+  const res = await fetch(`${process.env.BACKEND_URL}/api/makers/all`);
   const data = await res.json();
-
-  console.log(`Makers data fetched. Count: ${data.length}`);
 
   return {
     makers: data
